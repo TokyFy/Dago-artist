b5da53bc314283c034a6578000f5e3eba97d2ab7
toky <tokyfydev@gmail.com>
2022-09-19 22:00:42 +0300

    Add loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Navbar from "./components/Navbar";
 import Parallax from "./components/parallax";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    const loader = document.getElementById("loadingScreen")!;
+    if (loader) {
+      loader.remove();
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
